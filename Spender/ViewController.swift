7128af257120420442847579e44b650a19d08521
Stepan Kukharskyi <steveharski@gmail.com>
2021-03-19 11:43:31 -0400

    Create the Spender project

---
+++ Spender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spender
+//
+//  Created by Steve Harski on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
