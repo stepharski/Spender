25987168f127d361ab49e1b196ca63be9765ca88
Stepan Kukharskyi <steveharski@gmail.com>
2021-03-19 23:53:42 -0400

    -Create MainTabBar with custom middle button -Create OverviewVC, ProfileVC and AddItemVC -Create LaunchScreen -Add App Icon

---
+++ Spender/Navigation/MainTabBar.swift
@@ -0,0 +1,64 @@
+//
+//  MainTabBar.swift
+//  Spender
+//
+//  Created by Steve Harski on 3/19/21.
+//
+
+import UIKit
+
+class MainTabBar: UITabBar {
+
+    private var middleButton = UIButton()
+    
+    // MARK: - Constants
+    private let addButtonImage = #imageLiteral(resourceName: "addButtonTabBar")
+    private let tabBarBackgroundColor = #colorLiteral(red: 0.9564778209, green: 0.9609039426, blue: 0.971724093, alpha: 1)
+    private let selectedBarButtonColor = #colorLiteral(red: 0.2053239644, green: 0.4410848022, blue: 0.2368885279, alpha: 1)
+    private let unselectedBarButtonColor = #colorLiteral(red: 0.3989664614, green: 0.4103312194, blue: 0.4143762589, alpha: 1)
+    
+    // MARK: - LifeCycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupTabBar()
+        setupMiddleButton()
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if self.isHidden {
+            return super.hitTest(point, with: event)
+        }
+        
+        let from = point
+        let to = middleButton.center
+        
+        return sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)) <= 39 ? middleButton : super.hitTest(point, with: event)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        middleButton.center = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+    }
+    
+    // MARK: - Functionality
+    func setupTabBar() {
+        tintColor = selectedBarButtonColor
+        unselectedItemTintColor = unselectedBarButtonColor
+        barTintColor = tabBarBackgroundColor
+    }
+    
+    func setupMiddleButton() {
+        middleButton.frame.size = CGSize(width: 70, height: 70)
+        middleButton.layer.cornerRadius = 35
+        middleButton.setImage(addButtonImage, for: .normal)
+        middleButton.layer.masksToBounds = true
+        middleButton.center = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+        middleButton.addTarget(self, action: #selector(test), for: .touchUpInside)
+        addSubview(middleButton)
+    }
+    
+    // TODO: - Present AddItemVC
+    @objc func test() {
+        print("add button is pressed")
+    }
+}
