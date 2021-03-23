ce08b5bd290a4b01d730e8436ce3c75f7445eede
Stepan Kukharskyi <steveharski@gmail.com>
2021-03-23 15:21:43 -0400

    Update Categories and List buttons - Add category and list image assets - Add animated selection handling for category and list buttons - Add Constants extension for OverviewVC

--- Spender/ViewController/OverviewViewController.swift
+++ Spender/ViewController/OverviewViewController.swift
@@ -19,11 +19,22 @@ class OverviewViewController: UIViewController {
     @IBOutlet weak var categoriesButton: UIButton!
     @IBOutlet weak var listButton: UIButton!
     
+    // MARK: - Properties
+    fileprivate lazy var constants = Constants()
+    
+    // MARK: - @IBAaction
+    @IBAction func selectCategoriesView(_ sender: UIButton) {
+        updateCategoriesListButtons(for: sender)
+    }
+    
+    @IBAction func selectListView(_ sender: UIButton) {
+        updateCategoriesListButtons(for: sender)
+    }
     
     // MARK: - ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCustomNavBar()
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,11 +47,45 @@ class OverviewViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    // MARK: - UI Functionality
+    // MARK: - UI Helpers
+    func setupUI() {
+        setupCustomNavBar()
+        updateCategoriesListButtons(for: listButton)
+    }
+    
     func setupCustomNavBar() {
         customNavBarView.addShadow()
-        customNavBarView.addGradient(from: #colorLiteral(red: 0.8715779185, green: 0.9737123847, blue: 0.8044601083, alpha: 1), to: #colorLiteral(red: 0.5840260386, green: 0.6783261895, blue: 0.4990360141, alpha: 1), direction: .topLeftToBottomRight)
+        customNavBarView.addGradient(from: constants.gradientStartColor,
+                                     to: constants.gradientEndColor,
+                                     direction: constants.gradientDirection)
         categoriesButton.layer.cornerRadius = categoriesButton.frame.height * 0.5
         listButton.layer.cornerRadius = listButton.frame.height * 0.5
+    }
+    
+    func updateCategoriesListButtons(for button: UIButton) {
+        categoriesButton.isSelected = button == categoriesButton
+        listButton.isSelected = button == listButton
+        
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            guard let self = self else { return }
+            self.categoriesButton.backgroundColor = self.categoriesButton.isSelected
+                ? self.constants.selectedButtonColor
+                : self.constants.unselectedButtonColor
+            self.listButton.backgroundColor = self.listButton.isSelected
+                ? self.constants.selectedButtonColor
+                : self.constants.unselectedButtonColor
+        }
+    }
+}
+
+// MARK: - Constants
+fileprivate extension OverviewViewController {
+    struct Constants {
+        let gradientStartColor = #colorLiteral(red: 0.8715779185, green: 0.9737123847, blue: 0.8044601083, alpha: 1)
+        let gradientEndColor = #colorLiteral(red: 0.5840260386, green: 0.6783261895, blue: 0.4990360141, alpha: 1)
+        let gradientDirection = GradientDirection.topLeftToBottomRight
+        
+        let selectedButtonColor = #colorLiteral(red: 0.9490196078, green: 1, blue: 0.9333333333, alpha: 1)
+        let unselectedButtonColor = #colorLiteral(red: 0.7882352941, green: 0.8588235294, blue: 0.7333333333, alpha: 1)
     }
 }
